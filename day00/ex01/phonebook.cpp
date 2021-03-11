c16c87a893fe7e4567d145c81bae964aaafd6e18
Mouadz <mlasrite@student.1337.ma>
2021-03-11 19:11:17 +0100

    .

--- day00/ex01/phonebook.cpp
+++ day00/ex01/phonebook.cpp
@@ -1,27 +1,81 @@
-#include <iostream>
-#include <string>
+#include "phonebook.hpp"
 
-void add_contact()
+void add_contact(Contact obs)
 {
+    std::string line;
+
+    std::cout << "First Name: ";
+    std::cin >> line;
+    obs.set_first_name(line);
+
+    std::cout << "Last Name: ";
+    std::cin >> line;
+    obs.set_last_name(line);
+
+    std::cout << "Nickname: ";
+    std::cin >> line;
+    obs.set_nickname(line);
+
+    std::cout << "Login: ";
+    std::cin >> line;
+    obs.set_login(line);
+
+    std::cout << "Postal Address : ";
+    std::cin >> line;
+    obs.set_postal_adress(line);
+
+    std::cout << "Email Address: ";
+    std::cin >> line;
+    obs.set_email_address(line);
+
+    std::cout << "Phone Number: ";
+    std::cin >> line;
+    obs.set_phone_number(line);
+
+    std::cout << "Birthday Date: ";
+    std::cin >> line;
+    obs.set_birthday_date(line);
+
+    std::cout << "Favorite Meal : ";
+    std::cin >> line;
+    obs.set_favorite_meal(line);
+
+    std::cout << "Underwear Color: ";
+    std::cin >> line;
+    obs.set_underwear_color(line);
+
+    std::cout << "Darkest Secret: ";
+    std::cin >> line;
+    obs.set_darkest_secret(line);
+
+    std::cout << "\nContact was successfully added." << std::endl;
 }
 
-void search_contact()
+void search_contact(Contact obs, int index)
 {
+    std::cout << index << "|" << obs.get_first_name() << "|" << obs.get_last_name() << "|" << obs.get_nickname() << std::endl;
 }
 
 int main()
 {
     std::string line;
+    Contact obs[8];
 
     for (;;)
     {
+        std::cout << "- ";
         std::cin >> line;
         if (line.compare("EXIT") == 0)
             break;
         else if (line.compare("ADD") == 0)
-            add_contact();
+        {
+            add_contact(obs[0]);
+        }
         else if (line.compare("SEARCH") == 0)
-            search_contact();
+        {
+            for (int i = 0; i < 8; i++)
+                search_contact(obs[i], i);
+        }
     }
     return 0;
 }
--- day00/ex01/phonebook.hpp
+++ day00/ex01/phonebook.hpp
@@ -1,7 +1,49 @@
 #ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+#include <string>
+#include <iostream>
+
+class Contact
+{
+    std::string first_name;
+    std::string last_name;
+    std::string nickname;
+    std::string login;
+    std::string postal_adress;
+    std::string email_address;
+    std::string phone_number;
+    std::string birthday_date;
+    std::string favorite_meal;
+    std::string underwear_color;
+    std::string darkest_secret;
+
+public:
+    void set_first_name(std::string str) { first_name = str; }
+    void set_last_name(std::string str) { last_name = str; }
+    void set_nickname(std::string str) { nickname = str; }
+    void set_login(std::string str) { login = str; }
+    void set_postal_adress(std::string str) { postal_adress = str; }
+    void set_email_address(std::string str) { email_address = str; }
+    void set_phone_number(std::string str) { phone_number = str; }
+    void set_birthday_date(std::string str) { birthday_date = str; }
+    void set_favorite_meal(std::string str) { favorite_meal = str; }
+    void set_underwear_color(std::string str) { underwear_color = str; }
+    void set_darkest_secret(std::string str) { darkest_secret = str; }
+
+    std::string get_first_name() { return first_name; }
+    std::string get_last_name() { return last_name; }
+    std::string get_nickname() { return nickname; }
+    std::string get_login() { return login; }
+    std::string get_postal_adress() { return postal_adress; }
+    std::string get_email_address() { return email_address; }
+    std::string get_phone_number() { return phone_number; }
+    std::string get_birthday_date() { return birthday_date; }
+    std::string get_favorite_meal() { return favorite_meal; }
+    std::string get_underwear_color() { return underwear_color; }
+    std::string get_darkest_secret() { return darkest_secret; }
+};
 
 void add_contact();
-void search_contact();
+void search_contact(Contact obs, int index);
 
 #endif
