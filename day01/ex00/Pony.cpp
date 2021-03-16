f0b4de7252b25e5383aa9fdafffa1c0c92f79596
Mouadz <mlasrite@student.1337.ma>
2021-03-16 16:57:20 +0100

    .

--- day01/ex00/Pony.cpp
+++ day01/ex00/Pony.cpp
@@ -8,7 +8,22 @@ void ponyOnTheStack()
 
 void ponyOnTheHeap()
 {
-    Pony* zed = new Pony("Zeded");
+    Pony *zed = new Pony("Zeded");
     zed->hello("Zed from the Heap !");
     delete zed;
+}
+
+Pony::Pony(std::string name) : _name(name)
+{
+    std::cout << "Pony " << this->_name << " is born" << std::endl;
+}
+
+Pony::~Pony()
+{
+    std::cout << "Pony " << this->_name << " died" << std::endl;
+}
+
+void Pony::hello(std::string l)
+{
+    std::cout << l << std::endl;
 }
--- day01/ex00/Pony.hpp
+++ day01/ex00/Pony.hpp
@@ -5,24 +5,13 @@
 
 class Pony
 {
-    private:
-        std::string _name;
+private:
+    std::string _name;
 
-    public:
-        Pony(std::string name) : _name(name)
-        {
-            std::cout << "Pony " << this->_name << " is born" << std::endl;
-        }
-
-        ~Pony()
-        {
-            std::cout << "Pony " << this->_name << " died" << std::endl;
-        }
-
-        void hello(std::string l)
-        {
-            std::cout << l << std::endl;
-        }
+public:
+    Pony(std::string name);
+    ~Pony();
+    void hello(std::string l);
 };
 
 void ponyOnTheStack();
