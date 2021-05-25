c92f08b286d59ab967c5db869acad4ed74a6fa41
Mouadz <mlasrite@student.1337.ma>
2021-05-25 19:05:30 +0100

    .

---
+++ day07/ex02/Array.hpp
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mouadlas <mouadlas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 17:21:18 by mouadlas          #+#    #+#             */
+/*   Updated: 2021/05/25 19:05:05 by mouadlas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+#include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+    unsigned int _size;
+    T *_array;
+
+public:
+    Array();
+    Array(unsigned int n);
+    Array(const Array<T> &copy);
+    ~Array();
+    Array &operator=(const Array<T> &copy);
+    T &operator[](unsigned int a);
+    unsigned int size();
+};
+
+template <typename T>
+Array<T>::Array() : _size(0), _array(nullptr) {}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+    this->_size = n;
+    this->_array = new T[n];
+}
+
+template <typename T>
+unsigned int Array<T>::size()
+{
+    return this->_size;
+}
+
+template <typename T>
+Array<T>::Array(const Array<T> &copy)
+{
+    *this = copy;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(const Array<T> &copy)
+{
+    this->_size = copy._size;
+
+    if (!this->_array)
+        delete[] this->_array;
+
+    this->_array = new T[this->_size];
+    for (unsigned int i = 0; i < this->_size; i++)
+        this->_array[i] = copy._array[i];
+
+    return *this;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int a)
+{
+    if (a < this->_size)
+        return this->_array[a];
+    else
+        throw std::exception();
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+    if (this->_array)
+        delete[] this->_array;
+}
+
+#endif
---
+++ day07/ex02/main.cpp
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mouadlas <mouadlas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 17:20:55 by mouadlas          #+#    #+#             */
+/*   Updated: 2021/05/25 19:05:01 by mouadlas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+template <typename T>
+void print_array(Array<T> &arr)
+{
+    for (unsigned int i = 0; i < arr.size(); i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    Array<int> arrayInt1(10);
+    Array<int> arrayInt2(10);
+    Array<char> arrayChar1(15);
+    Array<char> arrayChar2(15);
+    try
+    {
+        {
+            std::cout << "Array Int 1 -> ";
+            print_array(arrayInt1);
+
+            std::cout << "Array Int 2 -> ";
+            print_array(arrayInt2);
+
+            arrayInt1[0] = 9;
+            arrayInt1[5] = 8;
+            std::cout << "Array Int 1 -> ";
+            print_array(arrayInt1);
+
+            arrayInt2 = arrayInt1;
+            std::cout << "Array Int 2 -> ";
+            print_array(arrayInt2);
+
+            Array<int> arrayInt3(arrayInt2);
+            std::cout << "Array Int 3 -> ";
+            print_array(arrayInt3);
+
+            arrayInt3[1] = 3;
+
+            std::cout << "Array Int 1 -> ";
+            print_array(arrayInt1);
+
+            std::cout << "Array Int 3 -> ";
+            print_array(arrayInt3);
+
+            Array<int> arrayInt4(50);
+
+            std::cout << "Array Int 4 -> ";
+            print_array(arrayInt4);
+
+            arrayInt4 = arrayInt3;
+
+            std::cout << "Array Int 4 -> ";
+            print_array(arrayInt4);
+        }
+        std::cout << "------" << std::endl;
+        {
+            arrayChar1[0] = 'A';
+            arrayChar1[5] = 'B';
+            std::cout << "Array Char 1 -> ";
+            print_array(arrayChar1);
+
+            arrayChar2 = arrayChar1;
+
+            std::cout << "Array Char 2 -> ";
+            print_array(arrayChar2);
+        }
+        std::cout << "------" << std::endl;
+        {
+            arrayInt1[98] = 8;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
